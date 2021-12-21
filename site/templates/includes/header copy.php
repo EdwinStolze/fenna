c8850eedff461fb99457c92d40b8552da54870aa
edwin <edwin@questic.nl>
2021-12-21 15:12:43 +0100

    lang switch

--- site/templates/includes/header copy.php
+++ site/templates/includes/header copy.php
@@ -1,19 +1,9 @@
 <header id="main-header">
-	<div class="l-container">
-		<ul>
-			<?php
-				// foreach($languages as $language) {
-				// 	if(!$page->viewable($language)) continue;
-				// 	if($language->id == $user->language->id) continue;
-				// 	$url = $page->localUrl($language); 
-				// 	echo "<li class=\"langswitch\"><a href='$url'>$language->title	</a></li>";
-				// }
-			?>
-		</ul>
-		<ul>
+	<div class="l-container l-clearfix t-margin">
+		<ul class="l-inline-list l-align-right">
 			<li class="social facebook"><a href="https://www.facebook.com/pages/Fenna-van-den-Berg-lifepersonal-coachingcounseling/666703320016240" target="_blank"></a></li>
 			<li class="social twitter"><a href="https://twitter.com/fennavdberg" target="_blank"></a></li>
 			<li class="social linkedIn"><a href="http://www.linkedin.com/pub/fenna-van-den-berg/81/984/72" target="_blank"></a></li>
 		</ul>
 	</div>
-</header>	
+</header>
